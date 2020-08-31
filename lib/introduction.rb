b793fe7e4abe4f881d0c4890bdedde92cd68f7b6
Jacob Short <jrshort89@gmail.com>
2020-08-31 15:38:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,9 @@ introduction_with_language(name, language)
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
+introduction_with_language_optional(name, language = "Ruby")
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
